bafbb30e87e524a21c0ff7febd450c28869e1c4f
Nithin S <s.nithin@muthoot.com>
2021-12-29 16:25:41 +0530

    read me

--- .NetFramework/InputCaching.net.framework/Properties/AssemblyInfo.cs
+++ .NetFramework/InputCaching.net.framework/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("InputCaching.net.framework.net.framework")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Solution to Input cache your .net framework web api")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("InputCaching.net.framework.net.framework")]
+[assembly: AssemblyCompany("NithinSuhas")]
+[assembly: AssemblyProduct("InputCaching.Net.framework.")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
--- .NetFramework/InputCaching.net.framework/docs/README.md
+++ .NetFramework/InputCaching.net.framework/docs/README.md
@@ -1,8 +1,9 @@
 # Input-Caching
+
 For handling input caching mechanism
 This Class library is developed using dot net core 6.
 
-Main purpose of this project is 
+Main purpose of this project is
 
 - To add caching mechanism for .net web api projects
 - To avoid duplicated post requests.
