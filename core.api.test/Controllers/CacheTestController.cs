2e4710ad5f0f97d49660de246af44850163064d8
Nithin S <s.nithin@muthoot.com>
2022-01-04 20:09:01 +0530

    Testings

---
+++ core.api.test/Controllers/CacheTestController.cs
@@ -0,0 +1,35 @@
+﻿using InputCaching.net.framework;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace core.api.test.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CacheTestController : ControllerBase
+    {
+      
+        // POST api/<ValuesController>
+        [HttpPost]
+        public IActionResult Post([FromBody] Data model)
+        {
+            var Collection = MemoryCache.Collection;
+            if (Collection.Push(model))
+            {
+                return Ok("Added");
+            }
+            else
+            {
+                return BadRequest("Already Added");
+            }
+        }
+
+        public class Data : MemoryObject
+        {
+            public int Id { get; set; }
+            public string Code { get; set; }
+        }
+    }
+}
